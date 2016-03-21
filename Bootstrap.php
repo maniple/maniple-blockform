39e5db25cde25432b62b2d014fb339cdbcacad68
xemlock <xemlock@gmail.com>
2016-03-21 21:07:35 +0100

    Use composer autoloading instead of ZF loader

--- Bootstrap.php
+++ Bootstrap.php
@@ -2,16 +2,6 @@
 
 class ManipleBlockform_Bootstrap extends Zefram_Application_Module_Bootstrap
 {
-    protected function _initAutoloader()
-    {
-        Zend_Loader_AutoloaderFactory::factory(array(
-            'Zend_Loader_StandardAutoloader' => array(
-                'prefixes' => array(
-                    'ManipleBlockform_' => dirname(__FILE__) . '/library/'
-                ),
-            ),
-        ));
-    }
     protected function _initView()
     {
         $bootstrap = $this->getApplication();
@@ -19,6 +9,6 @@ class ManipleBlockform_Bootstrap extends Zefram_Application_Module_Bootstrap
 
         /** @var Zend_View $view */
         $view = $bootstrap->getResource('View');
-        $view->addHelperPath(dirname(__FILE__) . '/library/View/Helper/', 'ManipleBlockform_View_Helper_');
+        $view->addHelperPath(__DIR__ . '/library/View/Helper/', 'ManipleBlockform_View_Helper_');
     }
 }
